badc4948de1f577ae0d8b0c9e755c18230c44e60
Stuart Russell <stuart.russell@protonmail.com>
2025-10-01 10:12:50 +0100

    add missing nur input

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
 
     home-manager = {
       url = "github:nix-community/home-manager";
